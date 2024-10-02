8d18bde6263a293e237fd22b3e9dd266965e8851
robin2616 <robinkumardbg11@gmail.com>
2024-10-02 10:29:06 +0530

    DSf

--- src/component/Mess.jsx
+++ src/component/Mess.jsx
@@ -55,8 +55,8 @@ console.log(x)
     return (<>
     
     {/* <span id="heading" className=" text-3xl w-[100vw] left-[13vw] fixed top-[-4vh] overflow-x-hidden overflow-y-hidden overflow-hidden flex justify-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">Manya Robin</span> */}
-    <h2 className=" text-3xl relative top-[2vh] left-[30vw] font-semibold ">Manya Robin</h2>
-      <div ref={chatBoxRef}  className="w-[90vw] h-[80vh] fixed top-[7vh] left-[5vw] right-[5vw] bg-blue-100 rounded-lg overflow-scroll custom-scrollbar ">
+    <h2 className=" text-3xl fixed top-[1vh] left-[30vw] font-semibold ">Manya Robin</h2>
+      <div ref={chatBoxRef}  className="w-[90vw] h-[80vh] fixed top-[6vh] left-[5vw] right-[5vw] bg-blue-100 rounded-lg overflow-scroll custom-scrollbar ">
     <div  class="space-y-4">
        <div className=" bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"></div>
 {x.map((i)=>(<>
@@ -77,7 +77,7 @@ console.log(x)
 
 
 
-        <div id="inp" class="form-floating shadow-lg  relative top-[88vh] w-[75vw] left-[5vw] h-[48px] rounded-lg">
+        <div id="inp" class="form-floating shadow-lg  fixed top-[87vh] translate-y-[-2px] w-[75vw] left-[5vw] h-[48px] rounded-lg">
             <input onKeyDown={handleKeyPress} type="text" value={c} onChange={(e)=>{d(e.target.value)}} class="form-control"   />
             <label for="floatingInput">Write Message</label>
         </div>
@@ -104,10 +104,10 @@ console.log(x)
 
 
         }}
-        class="btn shadow-lg btn-info fixed top-[88vh] translate-y-[1px] w-[15vw] left-[81vw] h-[45px]">Send</button> 
+        class="btn shadow-lg btn-info fixed bottom-[7vh]  w-[15vw] left-[81vw] h-[45px]">Send</button> 
         {/* <IoSend className=" text-2xl translate-x-6"/> */}
 
-<button className="text-2xl fixed top-[2vh]  w-[12vw] left-[81vw] h-[5vh] rounded-t-full bg-yellow-300 p-3 rounded-xl" onClick={()=>{location.reload()}}>g</button>
+<button className="text-2xl fixed top-[1vh]  w-[12vw] left-[81vw] h-[5vh] rounded-t-full bg-yellow-300 p-3 rounded-xl" onClick={()=>{location.reload()}}></button>
     </>)
 }
 export default Mess
